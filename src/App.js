07b50be479107341151a90c25e1f912fc718b6e5
wrfmartinez <wrmartinez709@gmail.com>
2024-06-12 15:52:59 -0400

    Add test for heading and adding a todo item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [todoItems, setTodoItems] = useState([]);
+
+  const addTodo = () => {
+    setTodoItems([...todoItems, userInput]);
+    setUserInput("");
+  }
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    setUserInput(inputValue);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Todooly</h1>
+      <input
+        type="text"
+        value={userInput}
+        placeholder='Enter a task here'
+        onChange={handleInputChange}
+      />
+      <button onClick={addTodo}>Add New</button>
+      <ul className='todo-list'>
+        {todoItems.map((todoItem, idx) => {
+          return <li className={idx}>{todoItem}</li>
+        })}
+      </ul>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the Todo heading', () => {
+  const app = render(<App />);
+  const heading = app.getByRole("heading")
+  expect(heading).toHaveTextContent("Todooly");
 });
+
+test('Able to add todo items', () => {
+  const app = render(<App />);
+  let todoItem = null;
+  const addButton = app.getByText("Add New");
+  const input = app.getByPlaceholderText("Enter a task here")
+
+  fireEvent.change(input, {target: {value: "New todo item"}});
+  fireEvent.click(addButton);
+  todoItem = app.getByText("New todo item");
+
+  expect(todoItem).toBeInTheDocument()
+});
+
+test('Able to delete a todo item', () => {
+  const app = render(<App />);
+  const todoItem = app.getByText("Todo item to delete");
+  const deleteButton = app.getByText("Delete");
+
+  fireEvent.click(deleteButton);
+
+  expect(todoItem).not.toBeInTheDocument();
+})
